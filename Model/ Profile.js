dd9769f09e5b4a3261e2b5a3e380bd83c06fec7e
romaine <romainelondoni@gmail.com>
2024-03-31 15:22:25 +0200

    Initial commit

---
+++ Model/ Profile.js
@@ -0,0 +1,9 @@
+
+let Profil={
+    id :1,  
+    poste:"Web Master",  
+    matricule : "123MA12",
+    user :  '',  
+    userId :1,
+}
+module.export={Profil}
---
+++ Model/user.js
@@ -0,0 +1,7 @@
+
+let User={
+    userName:'rlondoni',
+    UserMail:'romainelondoni@gmail.com',
+    password: '123442',
+    
+}
